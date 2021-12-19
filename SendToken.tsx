43f2fe4703166e7257f523a02425aab01321ee70
Kirk <kfournia@gmail.com>
2021-12-19 12:12:36 +0200

    Update SendToken.tsx
    Minor cleaning up

--- SendToken.tsx
+++ SendToken.tsx
@@ -346,7 +346,6 @@ export default function SendToken(props: any) {
                                     autoComplete="off"
                                     onChange={(e) => {setToAddress(e.target.value)}}
                                     InputProps={{
-                                        // step: 0.000000001, CONSIDER USING NUMBER TYPE (hide arrows with CSS)
                                         inputProps: {
                                             style: {
                                                 textAlign:'center'
